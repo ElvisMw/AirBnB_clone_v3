78d3c3dee71e5fd6ce02ebb6c920a386712817d1
ElvisMw <elvismwanthi@gmail.com>
2024-02-27 00:09:08 +0300

    fix import on v1/__init__.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-"""Index view for the API v1
-"""
-
+"""Index view for the API v1"""
 from api.v1 import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status', methods=['GET'])
 def get_status():
-    """Returns the status of the API
-    """
+    """Returns the status of the API"""
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
-    """Retrieves the number of each objects
-    """
+    """Retrieves the number of each object type"""
     stats = {
         "amenities": storage.count("Amenity"),
         "cities": storage.count("City"),
