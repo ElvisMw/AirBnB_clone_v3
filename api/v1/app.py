0660c4f5d70988908fc93657076219a25b40383a
ElvisMw <elvismwanthi@gmail.com>
2024-02-27 00:55:36 +0300

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-"""Main script to start the API"""
-from flask import Flask, jsonify, make_response
-from api.v1 import app_views
+""" Starts the Flask application """
+from flask import Flask, jsonify
 from models import storage
-import os
+from api.v1.views import app_views
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
 app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close_storage(exception):
+    """Closes the storage on teardown."""
+    storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """Handler for 404 errors."""
-    return make_response(jsonify({"error": "Not found"}), 404)
-
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the database again at the end of the request."""
-    storage.close()
+    """Handles 404 Not Found errors."""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
