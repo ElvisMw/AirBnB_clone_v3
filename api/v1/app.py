573605bce4d887afa14f5917f432829377f4cf96
ElvisMw <elvismwanthi@gmail.com>
2024-02-27 00:19:57 +0300

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,15 +8,18 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.errorhandler(404)
 def not_found(error):
     """Handler for 404 errors."""
     return make_response(jsonify({"error": "Not found"}), 404)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """Closes the database again at the end of the request."""
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
